d8d290945ea791d51fa5a0bcd74e66634810050e
mikezks <23002661+mikezks@users.noreply.github.com>
2022-04-01 09:10:13 +0200

    feat: custom filter operator

--- apps/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Flight } from '@flight-workspace/flight-lib';
-import { catchError, debounceTime, delay, distinctUntilChanged, EMPTY, filter, iif, map, Observable, of, share, Subscription, switchMap, tap, timer, withLatestFrom } from 'rxjs';
+import { catchError, debounceTime, delay, distinctUntilChanged, EMPTY, filter, iif, map, UnaryFunction, Observable, of, share, Subscription, switchMap, tap, timer, withLatestFrom, pipe } from 'rxjs';
 
 @Component({
   selector: 'flight-workspace-flight-typeahead',
@@ -31,10 +31,21 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
       city: 'Berlin'
     });
 
+    const customFilter = <T>(
+      filterFn: (value: T) => boolean,
+      debounceTimeValue: number
+    ): UnaryFunction<
+      Observable<T>, Observable<T>
+    > => pipe(
+      filter(filterFn),
+      debounceTime(debounceTimeValue),
+      distinctUntilChanged()
+    );
+
     /**
      * Stream 1: Input control -> value changes
      * - Trigger
-     * - No Data Provider
+     * - Data Provider
      */
     return this.control.valueChanges.pipe(
       // Get additional state
@@ -43,8 +54,12 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
       // map(([, state]) => state.city),
       // Filtering START
       // filter(city => city.length > 2),
-      debounceTime(300),
-      distinctUntilChanged(),
+      /* debounceTime(300),
+      distinctUntilChanged(), */
+      customFilter(
+        city => city.length > 2,
+        300
+      ),
       // Filtering END
       // Side-Effect
       tap(() => this.loading = true),
